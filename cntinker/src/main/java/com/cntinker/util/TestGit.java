ed7a3d5bed6f0632a26c6c0fc8b5b2ab0006341b
liubin <376068867@qq.com>
2015-01-24 15:45:24 +0800

    2

---
+++ cntinker/src/main/java/com/cntinker/util/TestGit.java
@@ -0,0 +1,15 @@
+/**
+ *2015年1月24日 下午3:37:20
+*/
+package com.cntinker.util;
+
+/**
+ * @author: liubin
+ *
+ */
+public class TestGit {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("");
+	}
+}
